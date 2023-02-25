1eaeabaaf51e840d536baf5cc76437d633c011e3
Serban <serban.crasovan@student.upt.ro>
2023-02-25 16:38:31 +0200

    Add hover functionality to the divs

--- jsscript.js
+++ jsscript.js
@@ -3,5 +3,11 @@ const container = document.querySelector("#container");
 		for (let i = 1; i <= 256; i++) {
 			const gridItem = document.createElement("div");
 			gridItem.classList.add("grid-item");
+
+      gridItem.addEventListener("mouseover", function() {
+        gridItem.style.backgroundColor = "#000";
+
+      })
+
 			container.appendChild(gridItem);
     }
